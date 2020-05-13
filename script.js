20a95de9cd031bd9507d65b5d95316a51205ea56
Orango Mango <61402409+OrangoMango@users.noreply.github.com>
2020-05-13 14:11:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var correct = Math.ceil(Math.random() * 100)
+var attemps = 0
+//alert(correct)
+
+function validate(){
+    var t = parseInt(document.getElementById("textinput").value, 10)
+    if (t == correct){
+      alert("Correct number in "+attemps+"attemps!")
+      return true
+    } else if (t < correct){
+      alert("Try higher")
+    } else if (t > correct){
+      alert("Try lower")
+    } else {
+        alert("Invalid input")
+    }
+    attemps++
+    return false
+}
